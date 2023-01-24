3c973bf6db65fdda393c570df008340d7a47df1d
supersakana <zacwilliamson@icloud.com>
2023-01-24 15:45:50 -0600

    wrapped up chapter 1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "rulers/version"
-
+require 'rulers/version'
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(_env)
+      [200, { 'Content-Type' => 'text/html' },
+       ['Hello from Ruby on Rulers!']]
+    end
+  end
 end
