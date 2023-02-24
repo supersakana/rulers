4aaffc5d209131a31c6b8b389cd8e9fbb2018ba0
supersakana <zacwilliamson@icloud.com>
2023-02-24 10:13:52 -0600

    added contoller class to our application along with get_controller_and_action method in our application

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
+
 require 'rulers/version'
 require 'rulers/array'
 
 module Rulers
   class Application
-    def call(_env)
-      `echo debug > debug.txt`
+    def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, { 'Content-Type' => 'text/html' },
-       ['Hello from Ruby on Rulers!']]
+       [text]]
     end
+
+    def get_controller_and_action(env)
+      _, cont, action, after =
+        env['PATH_INFO'].split('/', 4)
+      cont = cont.capitalize # "People"
+      cont += 'Controller' # "PeopleController"
+      [Object.const_get(cont), action]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    attr_reader :env
   end
 end
